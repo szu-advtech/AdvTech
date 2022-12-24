1ef45363b7f2792395feaa9485504fa59e720579
sunyang <108915791+thissunyang@users.noreply.github.com>
2022-12-24 13:00:32 +0800

    Add files via upload

---
+++ 2022/19-孙杨-2210275042 指导老师-陈杰/config/__init__.py
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+# This code was used and modified by Sun Yang on December 15, 2022.
+from .default import _C as cfg
+from .default import update_config
+from .default import check_config
