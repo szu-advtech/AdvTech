e785a28b131589fc52bd4be37f11c3c3e86a2933
Chandlers641 <59656468+Chandlers641@users.noreply.github.com>
2022-12-25 22:02:21 +0800

    Add files via upload

---
+++ 萧畅达-2200271024 指导老师-许智武/IntegratedLearning/src/main/Scala/DHBoost/Higges/DataPartition.scala
@@ -0,0 +1,61 @@
+package DHBoost.Higges
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object DataPartition {
+
+    def main(args: Array[String]): Unit = {
+        // 开始计时
+        val startTime = System.currentTimeMillis()
+
+        // 初始化参数
+        val L: Int = 3; //基学习器数量与分区数量一致
+        // 创建环境
+        val spark: SparkSession = SparkSession
+            .builder
+            .appName("DataPartition_Higges")
+            .getOrCreate()
+
+        // 对数据进行切分处理
+        println("DataPartition\n")
+
+        // 加载数据
+        val totalData: DataFrame = spark
+            .read
+            .format("csv")
+            .load("hdfs://172.31.238.20:8020/user/chandler/recurrence/higges/HIGGS.csv")
+
+        // 切分测试集训练集
+        val splitData = totalData.randomSplit(Array(0.3, 0.7))
+
+        splitData(0)
+            .repartition(1)
+            .write
+            .mode("overwrite")
+            .format("csv")
+            .save("hdfs://172.31.238.20:8020/user/chandler/recurrence/higges/test") // 测试数据集
+
+        splitData(1)
+            .repartition(1)
+            .write
+            .mode("overwrite")
+            .format("csv")
+            .save("hdfs://172.31.238.20:8020/user/chandler/recurrence/higges/train") // 训练数据集
+
+        // 数据分区
+        splitData(1)
+            .repartition(L)
+            .write
+            .mode("overwrite")
+            .format("csv")
+            .save("hdfs://172.31.238.20:8020/user/chandler/recurrence/higges/train/repartationRdd") // 手动分区，并进行重命名
+
+        // 停止计时
+        val endTime = System.currentTimeMillis()
+        val costTime = (endTime - startTime) / 1000
+        println("costTime = " + costTime)
+
+        // 停止
+        spark.stop()
+    }
+}
