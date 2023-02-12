fb8aa08d3a4fa7a5dd0393e1bb835fcd96e9f293
Sebastian Ma <sebastian.xq.ma@gmail.com>
2023-02-12 21:58:36 +0800

    Add files via upload

---
+++ 2022/01-马雪奇 指导老师-黄惠/code/undc_example.sh
@@ -0,0 +1,1 @@
+python main.py --checkpoint_dir weights_final --test_input examples/zhiteng.ply --input_type noisypc --method undc --postprocessing --point_num 524288 --grid_size 64 --block_num_per_dim 5 --gpu 1
