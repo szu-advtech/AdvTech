e0e01064e29d7c65516df89ef30d92e88e95e7bd
sunyang <108915791+thissunyang@users.noreply.github.com>
2023-02-15 20:08:24 +0800

    Delete 2022/19-孙杨-2210275042 指导老师-陈杰 directory

--- 2022/19-孙杨-2210275042 指导老师-陈杰/config/__init__.py
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------------------------------------
-# Copyright (c) Microsoft
-# Licensed under the MIT License.
-# Written by Bin Xiao (Bin.Xiao@microsoft.com)
-# ------------------------------------------------------------------------------
-# This code was used and modified by Yang Sun on December 15, 2022.
-from .default import _C as cfg
-from .default import update_config
-from .default import check_config
