fd8ceaee76fe797c3562d285c58fa93997e7508a
Lth_learning <49429171+Tian98Hang@users.noreply.github.com>
2023-02-16 09:45:22 +0800

    Delete 24-刘天航 指导老师-张磊 directory

--- 24-刘天航 指导老师-张磊/run_benchmark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-DIRNAME=$1
-
-node $BASEDIR/chrome/single_obj_test.js --log --dir $DIRNAME
-node $BASEDIR/chrome/multi_obj_test.js --log --multi --dir $DIRNAME
