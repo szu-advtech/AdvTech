3bafc99110ef060ed2b7d2d5b82c73216d5b396e
szu.advtech <118866731+szu-advtech@users.noreply.github.com>
2023-02-16 14:40:36 +0800

    Delete 2022/07-容芳林- 指导老师-廖好/07-容芳林- 指导老师-廖好/codes directory

--- 2022/07-容芳林- 指导老师-廖好/07-容芳林- 指导老师-廖好/codes/RACL-master/run.sh
+++
@@ -1,6 +0,0 @@
-python train_racl.py --task res14
-python train_racl.py --task lap14
-python train_racl.py --task res15
-python train_racl_bert.py --task res14
-python train_racl_bert.py --task lap14
-python train_racl_bert.py --task res15
